d85f671b20508c953daa8011937a6b673589e997
颜学宙 <>
2020-07-30 09:28:28 +0800

    添加横屏支持

--- suspensionPay/AppDelegate.m
+++ suspensionPay/AppDelegate.m
@@ -22,6 +22,7 @@
 
 
 #pragma mark - InterfaceOrientation //应用支持的方向
+
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
     return UIInterfaceOrientationMaskAllButUpsideDown;
 }
